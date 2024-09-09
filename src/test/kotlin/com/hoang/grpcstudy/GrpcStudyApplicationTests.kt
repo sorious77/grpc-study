ecba6e0a25c37543e804eac5a218bb879e76b4fd
sorious77 <sorious77@naver.com>
2024-09-09 22:30:39 +0900

    :tada: 프로젝트 생성

---
+++ src/test/kotlin/com/hoang/grpcstudy/GrpcStudyApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.hoang.grpcstudy
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GrpcStudyApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
